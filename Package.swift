d093757d7605d6b0be329bc294276978751457d0
Uhucream <aborog1324@yahoo.co.jp>
2023-06-29 21:13:18 +0900

    style: Remove unnecessary comments from Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftyCIImageProperties",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftyCIImageProperties",
             targets: ["SwiftyCIImageProperties"]),
@@ -15,8 +14,6 @@ let package = Package(
         .package(url: "https://github.com/swhitty/DictionaryDecoder", .upToNextMajor(from: "0.22.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftyCIImageProperties"),
         .testTarget(
