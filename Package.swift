f0d2f6f49769bef789bed6722d8ef23b69855799
Uhucream <aborog1324@yahoo.co.jp>
2023-06-29 21:11:25 +0900

    chore: Added a dependency of "DictionaryDecoder" to decode CIImage.properties dictionary

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "SwiftyCIImageProperties",
             targets: ["SwiftyCIImageProperties"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/swhitty/DictionaryDecoder", .upToNextMajor(from: "0.22.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
