49140693644cf5ac83a4bfbb1495094348bb5eaa
Uhucream <aborog1324@yahoo.co.jp>
2023-07-01 19:01:27 +0900

    feat: Add typealias of ImageIOProperties to CIImage to simplify codes for type annotations.

---
+++ Sources/SwiftyImageIO/Extensions/CoreImage/CIImage+Properties.swift
@@ -0,0 +1,13 @@
+//
+//  CIImage+Properties.swift
+//
+//  
+//  Created by TakashiUshikoshi on 2023/06/30
+//  
+//
+
+import CoreImage
+
+public extension CIImage {
+    typealias Properties = ImageIOProperties
+}
