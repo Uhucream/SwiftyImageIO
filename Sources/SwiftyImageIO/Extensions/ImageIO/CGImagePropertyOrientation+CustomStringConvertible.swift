c32370087da80ce6a8f366da22149ab2c0ff268b
Uhucream <aborog1324@yahoo.co.jp>
2023-07-02 00:05:17 +0900

    feat: Make `CGImagePropertyOrientation` debuggable
    Before adding this, CGImagePropertyOrientation print result was`__C.CGImagePropertyOrientation`, but by this change, `CGImagePropertyOrientation` prints case name.

---
+++ Sources/SwiftyImageIO/Extensions/ImageIO/CGImagePropertyOrientation+CustomStringConvertible.swift
@@ -0,0 +1,39 @@
+//
+//  CGImagePropertyOrientation+CustomStringConvertible.swift
+//
+//  
+//  Created by TakashiUshikoshi on 2023/07/01
+//  
+//
+
+import ImageIO
+
+extension CGImagePropertyOrientation: CustomStringConvertible {
+    public var description: String {        
+        switch self {
+        case .up:
+            return "up"
+            
+        case .upMirrored:
+            return "upMirrored"
+            
+        case .down:
+            return "down"
+            
+        case .downMirrored:
+            return "downMirrored"
+            
+        case .leftMirrored:
+            return "leftMirrored"
+            
+        case .right:
+            return "right"
+            
+        case .rightMirrored:
+            return "rightMirrored"
+            
+        case .left:
+            return "left"
+        }
+    }
+}
