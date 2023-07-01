09358e2d225bd1fb9e5d8ed0663ac5ea935fcca0
Uhucream <aborog1324@yahoo.co.jp>
2023-07-01 18:50:29 +0900

    feat: Create `DateFormatter` extension to parse exif date string.

---
+++ Sources/SwiftyImageIO/Extensions/Foundation/DateFormatter+.swift
@@ -0,0 +1,21 @@
+//
+//  DateFormatter+.swift
+//
+//  
+//  Created by TakashiUshikoshi on 2023/06/30
+//  
+//
+
+import Foundation
+
+extension DateFormatter {
+    static var tiff: DateFormatter {
+        let formatter: DateFormatter = .init()
+        
+        formatter.locale = NSLocale.system
+        formatter.dateFormat =  "yyyy:MM:dd HH:mm:ss"
+        
+        return formatter
+    }
+}
+
